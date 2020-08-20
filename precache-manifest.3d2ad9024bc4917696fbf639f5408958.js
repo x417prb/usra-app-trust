87e77d73c72f9764b30ec472098192ca098bce29
blfunex <blfunex@gmail.com>
2020-08-20 13:46:02 +0000

    Deploying to gh-pages from  @ 1459eb455353138d2e8d418adb45bce98dd29c66 🚀

---
+++ precache-manifest.3d2ad9024bc4917696fbf639f5408958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e09f347feff93908475f4cd635a022d",
+    "url": "/usra-app/index.html"
+  },
+  {
+    "revision": "d2d813833d40fc3518ff",
+    "url": "/usra-app/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "d2d813833d40fc3518ff",
+    "url": "/usra-app/static/js/2.236327c9.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/usra-app/static/js/2.236327c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5fdc4b400e334df2c7",
+    "url": "/usra-app/static/js/main.13c4377c.chunk.js"
+  },
+  {
+    "revision": "80d8c83f5a885d0483ab",
+    "url": "/usra-app/static/js/runtime-main.92ea8332.js"
+  }
+]);
