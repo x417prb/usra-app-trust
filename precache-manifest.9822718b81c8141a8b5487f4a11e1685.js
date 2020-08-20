e790aca69bc922c0fbf0b4f4555e8e1eb1fa5085
blfunex <blfunex@gmail.com>
2020-08-20 14:09:32 +0000

    Deploying to gh-pages from  @ 486a45653317a3da6de9490df3c9d723e19a5d4b 🚀

---
+++ precache-manifest.9822718b81c8141a8b5487f4a11e1685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61d97cd1ce4c7e829ba41064ec7d968",
+    "url": "/usra-app-trust/index.html"
+  },
+  {
+    "revision": "d2d813833d40fc3518ff",
+    "url": "/usra-app-trust/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "d2d813833d40fc3518ff",
+    "url": "/usra-app-trust/static/js/2.236327c9.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/usra-app-trust/static/js/2.236327c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0053dab2415e91040f0b",
+    "url": "/usra-app-trust/static/js/main.cf637300.chunk.js"
+  },
+  {
+    "revision": "8aca4c6b20d90395d7b5",
+    "url": "/usra-app-trust/static/js/runtime-main.2da07753.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/usra-app/precache-manifest.3d2ad9024bc4917696fbf639f5408958.js"
+  "/usra-app-trust/precache-manifest.9822718b81c8141a8b5487f4a11e1685.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/usra-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/usra-app-trust/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
