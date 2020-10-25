f477ac6f6548dddd689a4dd880af10738d4144a8
ox-ox <73203821+ox-ox@users.noreply.github.com>
2020-10-25 11:01:55 +0100

    Added button for setting menu

--- src/components/ChoiceBox.tsx
+++ src/components/ChoiceBox.tsx
@@ -19,20 +19,23 @@ export default function ChoiceBox({
     <div className="mb-3 row">
       <label className="offset-sm-2 col-sm-4 col-form-label">{label}:</label>
       <div className="col-sm-4">
-        <select
-          className="form-select form-select-sm"
-          value={value}
-          onChange={(e) => onChange(parseInt(e.target.value, 10))}
-        >
-          <option value={-1}></option>
-          {items.map((item, index) => {
-            return (
-              <option key={index} value={index}>
-                {item.name}
-              </option>
-            );
-          })}
-        </select>
+        <div className="input-group">
+          <select
+            className="form-select form-select-sm"
+            value={value}
+            onChange={(e) => onChange(parseInt(e.target.value, 10))}
+          >
+            <option value={-1}></option>
+            {items.map((item, index) => {
+              return (
+                <option key={index} value={index}>
+                  {item.name}
+                </option>
+              );
+            })}
+          </select>
+          <button className="btn btn-outline-secondary btn-sm">⋮</button>
+        </div>
       </div>
     </div>
   );
